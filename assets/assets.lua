1afdaccd78f421a137ec08b834989a9d5b58271b
Pablo Ariel Mayobre <pablo.a.mayobre@gmail.com>
2018-08-13 07:03:17 -0300

    O slice everything

---
+++ assets/assets.lua
@@ -0,0 +1,81 @@
+return {
+   ["prop"] = {
+      ox = 0, oy = 0, oz = 0,
+      w = 28, h = 28, layers = 28,
+
+      "prop-chair.png",
+   },
+   ["wallprop"] = {
+      ox = 0, oy = -6, oz = 0,
+      w = 20, h = 12, layers = 64,
+
+      "wallprop-torch.png",
+      "wallprop-chain.png",
+      "wallprop-column.png",
+      "wallprop-flag.png",
+      "wallprop-flag-t5.png",
+      "wallprop-flag-p7.png",
+   },
+   ["knight"] = {
+      ox = 0, oy = 6.5, oz = 0,
+      w = 31, h = 43, layers = 48,
+
+      "knight-idle.png",
+      "knight-sword-idle.png",
+
+      "knight-walk1.png",
+      "knight-sword-walk1.png",
+      "knight-stab-walk1.png",
+
+      "knight-walk2.png",
+      "knight-sword-walk2.png",
+      "knight-stab-walk2.png",
+
+      "knight-dash.png",
+   },
+   ["sword"] = {
+      ox = 0, oy = 6.5, oz = 0,
+      w = 31, h = 43, layers = 58,
+
+      "sword-inplace.png",
+      "sword-grabbed.png",
+      "sword-raised.png",
+      "sword-empty.png",
+   },
+   ["skeleton"] = {
+      ox = 0, oy = 10.5, oz = 0,
+      w = 25, h = 33, layers = 48,
+
+      "skeleton-idle.png",
+      "skeleton-stab.png",
+
+      "skeleton-walk1.png",
+      "skeleton-walk2.png",
+
+      "skeleton-dead1.png",
+      "skeleton-dead2.png",
+   },
+   ["table"] = {
+      ox = 0, oy = 0, oz = 0,
+      w = 64, h = 32, layers = 24,
+
+      "table-empty.png",
+      "table-cake.png",
+   },
+   ["wall"] = {
+      ox = 0, oy = 0, oz = 0,
+      w = 48, h = 48, layers = 64,
+
+      "wall-normal.png",
+   },
+   ["tile"] = {
+      ox = 0, oy = 0, oz = 1,
+      w = 48, h = 48, layers = 2,
+
+      "tile-floor1.png",
+      "tile-floor2.png",
+      "tile-floor3.png",
+
+      "tile-ceiling.png",
+   }
+}
